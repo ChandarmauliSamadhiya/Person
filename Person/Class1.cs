55004218279f65fa1de7ae8fe02e2989c4ec0b5a
acer <acer@ASUR>
2023-08-21 09:14:02 +0530

    Add project files.

---
+++ Person/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public partial class Person
+{
+    public void PrintFullName()
+    {
+        Console.WriteLine($"Full Name: {FirstName} {LastName}");
+    }
+}
+
---
+++ Person/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person();
+        person.FirstName = "John";
+        person.LastName = "Doe";
+
+        person.PrintFullName();
+        Console.ReadLine();
+    }
+}
+
