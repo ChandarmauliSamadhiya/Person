d045760c140f84778c85f30e351d4360ab7769bc
ChandarmauliSamadhiya <142165518+ChandarmauliSamadhiya@users.noreply.github.com>
2023-08-28 09:25:15 +0530

    Update Class2.cs

--- Person/Class2.cs
+++ Person/Class2.cs
@@ -6,6 +6,6 @@ using System.Threading.Tasks;
 
 public partial class Person
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+    public string FirstName ;
+    public string LastName ;
 }
